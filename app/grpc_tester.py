f342fa449b13856e549fc42bd66ee23ced043fb5
bogdan artyushenko <bogdan.artyushenko@gmail.com>
2019-11-28 17:25:08 +0200

    g.c.: simple test

--- app/grpc_tester.py
+++ app/grpc_tester.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
   with grpc.insecure_channel('grpc-server:80') as channel:
       stub = analysis_pb2_grpc.AnalysisServiceStub(channel)
       feature = stub.Analyze(msg)
-      if feature.text != msg.text:
+      if feature.text == 'my phone number is phone-number and my credit card is credit-cart':
           print('PASSED', feature)
           sys.exit(0)
       else:
