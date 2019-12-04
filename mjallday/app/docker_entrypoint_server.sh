99d9d4274ba78e2d3c7b43b89a31c071181ae054
Bogdan Artyushenko <bogdan.artyushenko@gmail.com>
2019-12-04 14:50:43 +0200

    g.c.: added requirement.txt

--- mjallday/app/docker_entrypoint_server.sh
+++ mjallday/app/docker_entrypoint_server.sh
@@ -9,7 +9,7 @@ curl -H "Content-Type: application/json"  -X POST -d '{"fieldTypeTransformations
 curl -H "Content-Type: application/json"  -X POST -d '{"text":"We met yesterday morning in Seattle and his phone number is (212) 555 1234", "AnalyzeTemplateId":"trashbaanalyzer"  }'  http://presidio-api:8080/api/v1/templates/1/analyze
 curl  -H "Content-Type: application/json" -v -X POST -d '{"text":"We met yesterday morning in Seattle and his phone number is (212) 555 1234", "AnalyzeTemplateId":"trashbaanalyzer", "AnonymizeTemplateId": "trashbaanonymizer"  }'  http://presidio-api:8080/api/v1/projects/1/anonymize
 
-python app/grpc_server.py >> /var/log/grpc_server.log
+python /app/grpc_server.py >> /var/log/grpc_server.log
 
 
 
